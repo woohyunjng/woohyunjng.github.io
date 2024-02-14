aa7916e2b2e4cf90e577a0e7f5ffa0ae53d9f6dd
woohyunjng <woohyun.jng08@gmail.com>
2024-02-14 13:02:45 +0900

    feat(layouts/achievements): add youtube

--- src/layouts/achievements.tsx
+++ src/layouts/achievements.tsx
@@ -1,33 +1,57 @@
+import YouTube from "react-youtube";
+
 import data from "../data.json";
 
 import "../styles/layouts/achievements.css";
 
 const Achievements = () => (
     <div className="achievements">
-        <h1 className="title titleFont">{data.achievements.title}</h1>
-        <h4 className="subTitle smallFont">{data.achievements.subtitle}</h4>
+        <h1 className="title toRight titleFont">{data.achievements.title}</h1>
+        <h4 className="subTitle toRight smallFont">
+            {data.achievements.subtitle}
+        </h4>
 
         <div className="list">
             <div className="element informatics">
-                <div className="type smallFont">Informatics</div>
+                <div className="type toRight smallFont">Informatics</div>
                 {data.achievements.list
                     .filter((x) => x.type === "informatics")
                     .map((x) => (
-                        <h4 className="listElement smallFont">
+                        <h4
+                            className="listElement toRight smallFont"
+                            key={x.name}
+                        >
                             {x.name} <b className="value">{x.value}</b> ←
                         </h4>
                     ))}
             </div>
             <div className="element math">
-                <div className="type smallFont">Math</div>
+                <div className="type toRight smallFont">Math</div>
                 {data.achievements.list
                     .filter((x) => x.type === "math")
                     .map((x) => (
-                        <h4 className="listElement smallFont">
+                        <h4
+                            className="listElement toRight smallFont"
+                            key={x.name}
+                        >
                             {x.name} <b className="value">{x.value}</b> ←
                         </h4>
                     ))}
             </div>
+        </div>
+
+        <h1 className="title titleFont">{data.achievements.youtubeTitle}</h1>
+        <div className="youtubeContainer">
+            <YouTube
+                videoId={data.achievements.youtubeOpts.id}
+                opts={{
+                    playerVars: {
+                        loop: 1,
+                        start: data.achievements.youtubeOpts.start,
+                    },
+                }}
+                onEnd={(e) => e.target.stopVideo(0)}
+            />
         </div>
     </div>
 );
